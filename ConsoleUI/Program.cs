0ec1e36092ec1ea6866f156e142c2c832490e4ec
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-01-04 15:59:52 +0500

    Exercice

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleUI
         {
             // Intialize a list
             List<string> firstNames = new List<string>();
+            List<string> students = new List<string>();
 
             firstNames.Add("John");
             firstNames.Add("Doe");
@@ -23,7 +24,24 @@ namespace ConsoleUI
             Console.WriteLine($"The second element is {firstNames[1]}");
             firstNames[1] = firstNames[1].ToUpper();
             Console.WriteLine($"The second element is {firstNames[1]}");
+
+            Console.Write("Is there any student left?(yes/no)");
+            string addNewStudent = Console.ReadLine();
+            int count_ = 0;
+
+            while (addNewStudent.ToLower() == "yes")
+            {
+                Console.Write("What is the students name: ");
+                string studentName = Console.ReadLine();
+                students.Add(studentName);
+                count_ += 1;
+                Console.Write("Is there any student left?(yes/no)");
+                addNewStudent = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The total of students is {count_}");
             Console.ReadLine();
+
         }
     }
 }
